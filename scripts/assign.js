688ce2133582e5c27c7381c30e7451f43eae2ead
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-10-07 21:06:40 -0700

    established a basic setup for assign.js.

--- scripts/assign.js
+++ scripts/assign.js
@@ -20,19 +20,23 @@ var FlowNetwork = function(users, shifts){
 	this.network.sink = {edges: []};
 	
 	//assign all shifts and edges to network
-	for ( var key in shifts ){
-		var shiftKey = shifts[key][name].split(' ').join('');
-		this.network[shiftKey] = shifts[key];
-		//Add edge from shift to sink
-		this.addEdge(shiftKey, 'sink', 1);
-	}
-	// assign keys and edges to network
-	for ( var key in users ){ 
-		var userKey = users[key][name].split(' ').join('');
-		// assign users to network to be saved by their full name
-		this.network[userKey] = users[key];
-		// Add edge to users 
-		this.addEdge('source', userKey, this.network[userKey][shiftsDesired]);
+	this.assignEdges = function(){
+		for ( var key in shifts ){
+			var shiftKey = shifts[key][name].split(' ').join('');
+			this.network[shiftKey] = shifts[key];
+			//Add edge from shift to sink
+			this.addEdge(shiftKey, 'sink', 1);
+		}
+		// assign keys and edges to network
+		for ( var key in users ){ 
+			var userKey = users[key][name].split(' ').join('');
+			// assign users to network to be saved by their full name
+			this.network[userKey] = users[key];
+			// Add edge to users 
+			this.addEdge('source', userKey, this.network[userKey][shiftsDesired]);
+		}
+
+		//TODO: assign user edges to shifts using the addEdge function.  This functionality should be contained elsewhere
 	}
 
 	this.addEdge = function(from, to, capacity){
@@ -43,6 +47,48 @@ var FlowNetwork = function(users, shifts){
 		this.network[from].push(newEdge);
 		this.network[to].push(newEdge);
 
+	};
+
+	this.removeSameDayShifts = function(){
+
+	};
+
+	this.addFlowToPath = function(path){
+		// add one unit of flow to each item in the path
+		// for Forward edges, add one flow
+		// for Reverse edges, remove one flow 
+	}
+
+	//recursive function used to find a path through the graph
+	this.findPath = function(currentNodeKey, sink, path){
+		// if currentNode is equal to sink
+		if (currentNodeKey === sink ){
+			return path;
+		}
+
+	}
+
+	// main function used to find the flow in the network
+	this.findMaxFlow = function(){
+		// Path variable used to store the current path
+		var path = this.findPath('source', 'sink', []);
+		
+		// while an augmenting path still exists
+		while( path != null ){
+			// we add flow to the path one at a time using our this.addFlowToPath function
+			this.addFlowToPath(path);
+			path = this.findPath('source', 'sink', []);
+		}
+
+		var shiftsAssigned = 0;
+		for ( var i = 0; i < this.network.source.edges.length; i++ ){
+			shiftsAssigned += this.network.source.edges[i].flow;
+		}
+		// assign shiftsAssigned variable to network for later reference
+		this.network['shiftsAssigned'] = shiftsAssigned;
+
+		// return the Filled network, which we will use to determine to whom shifts were assigned
+		return this.network;
 	}
 
 
