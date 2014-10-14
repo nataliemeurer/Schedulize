8c2afae71c231bd3eee5426050a6d48c87d098ee
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-10-13 19:23:18 -0700

    Began creating partials for angular router.

--- public/scripts/user/app.js
+++ public/scripts/user/app.js
@@ -1,65 +1,53 @@
-angular.module('shortly', [
-  'shortly.services',
-  'shortly.links',
-  'shortly.shorten',
-  'shortly.auth',
-  'ngRoute'
-])
-.config(function($routeProvider, $httpProvider) {
-  $routeProvider
-    .when('/signin', {
-      templateUrl: 'app/auth/signin.html',
-      controller: 'AuthController'
+var userApp = angular.module('userApp', [
+  'ui.router',
+]);
+
+console.log("executing");
+userApp.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+  $stateProvider
+    .state('dashboard', {
+      url: "/dashboard",
+      templateUrl: "partials/dashboard.html"
     })
-    .when('/signup', {
-      templateUrl: 'app/auth/signup.html',
-      controller: 'AuthController'
+    .state('userprofile', {
+      url: "/profile",
+      templateUrl: "partials/userprofile.html"
     })
-    .when('/links', {
-      templateUrl: 'app/links/links.html',
-      authenticate: true,
-      controller: 'LinksController'
-    })
-    .when('/shorten', {
-      templateUrl: 'app/shorten/shorten.html',
-       authenticate: true,
-      controller: 'ShortenController'
-    })
-    .otherwise({
-      redirectTo: '/links'
+    .state('setavailability', {
+      url: "/availability",
+      templateUrl: "partials/availability.html"
     });
-    // We add our $httpInterceptor into the array
-    // of interceptors. Think of it like middleware for your ajax calls
-    $httpProvider.interceptors.push('AttachTokens');
-})
-.factory('AttachTokens', function ($window) {
-  // this is an $httpInterceptor
-  // its job is to stop all out going request
-  // then look in local storage and find the user's token
-  // then add it to the header so the server can validate the request
-  var attach = {
-    request: function (object) {
-      var jwt = $window.localStorage.getItem('com.shortly');
-      if (jwt) {
-        object.headers['x-access-token'] = jwt;
-      }
-      object.headers['Allow-Control-Allow-Origin'] = '*';
-      return object;
-    }
-  };
-  return attach;
-})
-.run(function ($rootScope, $location, Auth) {
-  // here inside the run phase of angular, our services and controllers
-  // have just been registered and our app is ready
-  // however, we want to make sure the user is authorized
-  // we listen for when angular is trying to change routes
-  // when it does change routes, we then look for the token in localstorage
-  // and send that token to the server to see if it is a real user or hasn't expired
-  // if it's not valid, we then redirect back to signin/signup
-  $rootScope.$on('$routeChangeStart', function (evt, next, current) {
-    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
-      $location.path('/signin');
-    }
-  });
+  
+  $urlRouterProvider.otherwise("/user");
 });
+// .factory('AttachTokens', function ($window) {
+//   // this is an $httpInterceptor
+//   // its job is to stop all out going request
+//   // then look in local storage and find the user's token
+//   // then add it to the header so the server can validate the request
+//   var attach = {
+//     request: function (object) {
+//       var jwt = $window.localStorage.getItem('com.shortly');
+//       if (jwt) {
+//         object.headers['x-access-token'] = jwt;
+//       }
+//       object.headers['Allow-Control-Allow-Origin'] = '*';
+//       return object;
+//     }
+//   };
+//   return attach;
+// });
+// .run(function ($rootScope, $location, Auth) {
+//   // here inside the run phase of angular, our services and controllers
+//   // have just been registered and our app is ready
+//   // however, we want to make sure the user is authorized
+//   // we listen for when angular is trying to change routes
+//   // when it does change routes, we then look for the token in localstorage
+//   // and send that token to the server to see if it is a real user or hasn't expired
+//   // if it's not valid, we then redirect back to signin/signup
+//   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+//     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+//       $location.path('/signin');
+//     }
+//   });
+// });
