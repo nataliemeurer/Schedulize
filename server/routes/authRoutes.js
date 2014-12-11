dfc304e08242430c74b088dd6579c122ef4a8174
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-12-10 23:53:54 -0800

    clean up auth file.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,5 @@
 var express = require('express');
-var router = express.Router();
 var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/schedulize');
-var companies = db.get('companies');
-var users = db.get('users');
 var bcrypt = require('bcrypt');
 
 module.exports = function (app) {
@@ -18,22 +13,23 @@ module.exports = function (app) {
     var admin = req.body.admin;
 
     bcrypt.hash(password, 8, function(err, hash){
-      users.insert({ name: name,
-                     email: email,
-                     password: hash,
-                     company: company,
-                     admin: admin, availability: [],
-                     shiftsDesired: null,
-                     canManage: null
-                   }, function (err, doc) {
-                        if (err) {throw err};
-                        req.session.regenerate(function(){
-                          req.session.name = name;
-                          req.session.company = company;
-                          res.redirect('/user');
-                          res.send(200);
-                        });
-                  });
+      users.insert(
+        { name: name,
+          email: email,
+          password: hash,
+          company: company,
+          admin: admin, availability: [],
+          shiftsDesired: null,
+          canManage: null
+         }, function (err, doc) {
+            if (err) {throw err};
+            req.session.regenerate(function(){
+              req.session.name = name;
+              req.session.company = company;
+              res.redirect('/user');
+              res.send(200);
+            });
+          });
     });
   });
 
