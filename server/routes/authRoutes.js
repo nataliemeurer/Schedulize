fcfce8ce35a07b576be03d40d6a2a39bd9602c6d
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-12-21 20:52:46 -0800

    Update and fix buttons.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,37 +1,8 @@
-var express = require('express');
-var mongo = require('mongodb');
-var bcrypt = require('bcrypt');
+var authController = require('../controllers/authController');
 
 module.exports = function (app) {
-  app.post('/signup', function(req, res){
-    console.log('signing you up!');
-    var name = req.body.name;
-    var username = req.body.usrname;
-    var email = req.body.email;
-    var password = req.body.password;
-    var company = req.body.company;
-    var admin = req.body.admin;
-
-    bcrypt.hash(password, 8, function(err, hash){
-      // users.insert(
-      //   { name: name,
-      //     email: email,
-      //     password: hash,
-      //     company: company,
-      //     admin: admin, availability: [],
-      //     shiftsDesired: null,
-      //     canManage: null
-      //    }, function (err, doc) {
-      //       if (err) {throw err};
-      //       req.session.regenerate(function(){
-      //         req.session.name = name;
-      //         req.session.company = company;
-      //         res.redirect('/user');
-      //         res.send(200);
-      //       });
-      //     });
-    });
-  });
+  app.route('/signup/employee')
+    .post();
 
   app.post('/signin', function(req, res){
 
