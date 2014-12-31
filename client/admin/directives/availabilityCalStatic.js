08931a27d8544b4e95edfa9e20402be6f03b1e83
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-12-31 12:47:34 -0800

    Create basic availability calendar that is static.

---
+++ client/admin/directives/availabilityCalStatic.js
@@ -0,0 +1,40 @@
+// 'use strict';
+var adminApp = angular.module('adminApp');
+
+adminApp.directive('availabilityCalStatic', function($http){
+  return {
+    restrict: 'A',
+    template: '<div class="calendar">' +
+              '<div id="availabilityCalStatic"></div>' +
+              '</div>',
+    replace: true,
+    link: function(scope, element, attrs) {
+      // Get all properties passed in from the DOM
+      var properties = scope.$eval(attrs.availabilityCalStatic);
+      // Create a select Function to handle adding availability to the calendar
+      properties.defaultDate = moment(1396310400000).utc();
+      properties.header = {left: '', right: ''};
+      properties.timezone = 'UTC';
+      properties.defaultView = 'agendaWeek';
+      properties.columnFormat = {week: 'dddd'};
+      properties.allDaySlot = false;
+      properties.selectHelper = true;
+      properties.overlap = false;
+      properties.firstHour = 6;
+      properties.unselectAuto = false;
+      properties.editable = false;
+      properties.eventDurationEditable = false;
+      properties.selectable = false;
+      
+      // LOAD SHIFTS AFTER PAGE HAS LOADED
+      scope.loadEvents = function(){
+        var shifts = scope.activeSchedule.shifts;
+        for(var i = 0; i < scope.activeSchedule.shifts.length; i++){
+          shifts[i]._id = null; // remove fc _id property which ruins everything.
+        }
+        properties.events = shifts;
+        $('#availabilityCalStatic').fullCalendar(properties);
+      };
+    }
+  };
+});
