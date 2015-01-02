1eb2f87cde04623ce9ef34752724dd9a851f0a75
Kevin Meurer <kevin.a.meurer@gmail.com>
2015-01-01 23:00:37 -0800

    update userview for easier availability viewing.

--- client/admin/controllers/employeemanager.controller.js
+++ client/admin/controllers/employeemanager.controller.js
@@ -15,10 +15,29 @@ adminApp
 .controller('employeeViewCtrl', function($scope, $http, $stateParams, $location, Employee){
 	Employee.getOneEmployee($stateParams.employeeId).then(function(employee){
 		$scope.employee = employee;
+		console.log(employee);
 		$scope.viewMode = "overview";
 		$scope.setMode = function(mode){
-			$scope.viewMode = mode;
+			$scope.viewMode = mode; 
 		}
 		$scope.loadEvents();
 	});
+
+	$scope.makeAdmin = function(){
+		swal({ 
+		  title: "Are you sure?", 
+		  text: "Making this user an admin will give them all of your administrative privileges.",
+		  type: "warning",
+		  showCancelButton: true,
+		  confirmButtonColor: "#DD6B55",
+		  confirmButtonText: "Yes, give them the power!",
+		  closeOnConfirm: false 
+		}, function(){
+		  swal("Success!", $scope.employee.name + " is now an administrator.", "success");
+		});
+	};
+
+	$scope.removeEmployee = function(){
+
+	};
 });
