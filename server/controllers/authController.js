744f5c9a062906f5e0921637c73961482fdca591
Kevin Meurer <kevin.a.meurer@gmail.com>
2015-01-28 09:49:42 -0500

    fixed login issue.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -35,55 +35,57 @@ module.exports = {
 
   createNewEmployee: function(req, res){
 		if(req.body.password !== req.body.password2){
-			res.render('public/signup', {error: "Passwords do not match"});
+			res.redirect('/signup');
 		} else {
       // Determine if company exists
       Company.findOne({name: req.body.companyName}, function(err, company){
         if(!company){ 
-          res.render('public/signup', {error: "The company you specified does not exist."});
-        }
-        // check company access key
-        bcrypt.compare(req.body.companyAccessKey, company.accessKey, function(err, result) {
-          if(!result){
-            res.render('public/signup', {error: "Invalid Access Key for " + company.name});
-          }
-          // Hash the given password
-          bcrypt.genSalt(10, function(err, salt){
-            bcrypt.hash(req.body.password, salt, function(err, hash) {
-              if(err){
-                console.log('Failed to hash password');
-                res.status(500).send(err);
-              }
-              // if valid key, create a new user
-              var newUser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                password: hash,
-                companies: [company._id],
-                shiftsDesired: null,
-                shiftsAssigned: 0,
-                availability: null,
-                eligibleRoles: [],
-                isAdmin: false,
-                joinDate: new Date()
-              });
-              // Save the new user
-              newUser.save(function (err, user) {
-                if (err) {
-                  res.status(500).send(err);
-                }
-                // Add user to list of company employees
-                company.employees.push(user._id);
-                company.save(function(err, updatedCompany){
-                  // log this user in
-                  passport.authenticate('local')(req, res, function () {
-                    res.redirect('/user');
+          res.redirect('/signup');
+        } else {
+          // check company access key
+          bcrypt.compare(req.body.companyAccessKey, company.accessKey, function(err, result) {
+            if(!result){
+              res.redirect('/signup');
+            } else {
+              // Hash the given password
+              bcrypt.genSalt(10, function(err, salt){
+                bcrypt.hash(req.body.password, salt, function(err, hash) {
+                  if(err){
+                    console.log('Failed to hash password');
+                    res.status(500).send(err);
+                  }
+                  // if valid key, create a new user
+                  var newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hash,
+                    companies: [company._id],
+                    shiftsDesired: null,
+                    shiftsAssigned: 0,
+                    availability: null,
+                    eligibleRoles: [],
+                    isAdmin: false,
+                    joinDate: new Date()
+                  });
+                  // Save the new user
+                  newUser.save(function (err, user) {
+                    if (err) {
+                      res.status(500).send(err);
+                    }
+                    // Add user to list of company employees
+                    company.employees.push(user._id);
+                    company.save(function(err, updatedCompany){
+                      // log this user in
+                      passport.authenticate('local')(req, res, function () {
+                        res.redirect('/user');
+                      });
+                    });
                   });
                 });
               });
-            });
+            }
           });
-        });      
+        }   
       });
 		}
 	},
